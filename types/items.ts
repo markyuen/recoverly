7c505fb4a368778b6a5693dbf5c50e0f5174e301
Ivan Leo <ivanleomk@gmail.com>
2022-02-03 00:04:49 +0800

    Fixed up typescript errors

--- types/items.ts
+++ types/items.ts
@@ -30,12 +30,12 @@ export type ItemPageData = {
   product_name: string;
   product_images: { url: string }[];
   product_specifications: { url: string }[];
-  product_categories: {
+  products_categories: {
     main_category: boolean;
     category: {
       category_name: string;
     };
-  };
+  }[];
   variations: {
     variation_1: string;
     variation_2: string;
