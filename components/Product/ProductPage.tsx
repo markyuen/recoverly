88884f1e451b2f8375dac2497da1abd64b758756
Ivan Leo <ivanleomk@gmail.com>
2022-02-11 16:06:01 +0800

    Fixed up a discount tag

--- components/Product/ProductPage.tsx
+++ components/Product/ProductPage.tsx
@@ -1,3 +1,4 @@
+import { Tag } from "@chakra-ui/react";
 import React from "react";
 import { ProductInformation } from "../../types/product";
 import ProductCategories from "./ProductCategories";
@@ -25,6 +26,27 @@ const ProductPage = ({ product }: ProductPageProps) => {
     variations,
   } = product;
 
+  const calculateDiscount = (original_price, discounted_price) => {
+    if (original_price == discounted_price) {
+      return 0;
+    }
+    return parseFloat(
+      ((100 * (original_price - discounted_price)) / original_price).toFixed(2)
+    );
+  };
+
+  const getMaxDiscountPercentage = () => {
+    const maxDiscount = variations.reduce(
+      (acc, item) =>
+        Math.max(
+          acc,
+          calculateDiscount(item.original_price, item.discounted_price)
+        ),
+      0
+    );
+    return Math.round(maxDiscount - (maxDiscount % 5));
+  };
+
   return (
     <div className="w-full max-w-4xl rounded   mx-auto text-gray-800 relative md:text-left">
       <div className="md:flex items-center -mx-10">
@@ -32,6 +54,15 @@ const ProductPage = ({ product }: ProductPageProps) => {
         <div className="w-full md:w-1/2 px-10">
           <h1 className="text-xl font-bold">{product_name}</h1>
           <p className="text-gray-700 text-sm">{brand_name}</p>
+          <Tag
+            style={{
+              backgroundColor: "#002570",
+              color: "white",
+            }}
+            className="my-2"
+          >
+            Up to {getMaxDiscountPercentage()}% Off
+          </Tag>
           <ProductVariations
             product_id={product_id}
             product_name={product_name}
